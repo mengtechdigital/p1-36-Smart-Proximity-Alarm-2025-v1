58794f65a07634ccfdd35af8a9eda71c7d0c2a3a
Randy See <mengtechdigital@gmail.com>
2025-10-21 10:51:25 +0800

    Update sound sample to drum cymbal and add playback functionality

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pygame
+import time
+
 pygame.init()
 
-my_sound = pygame.mixer.Sound('./sound samples/bd_fat.wav')
+my_sound = pygame.mixer.Sound(
+    "/Users/randy/development/p1-36-Smart-Proximity-Alarm-2025-v1/sound samples/drum_cymbal_hard.wav"
+)
+
+my_sound.play()
+
+time.sleep(3)
